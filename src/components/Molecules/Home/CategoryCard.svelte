4b53832fcb4f09761995af3c8fd7b0fae1f8e0d8
Irving Suárez <irving.dev@icloud.com>
2020-09-30 17:35:43 -0500

    Feature: Adding category card

---
+++ src/components/Molecules/Home/CategoryCard.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let { name, image, description };
+</script>
+
+<style>
+</style>
+
+<div class="col">
+  <div class="card">
+    <div class="card-img-top"><img src={image} alt="" /></div>
+    <div class="card-body">
+      <h5 class="card-title">{name}</h5>
+      <p class="card-text">{description}</p>
+    </div>
+  </div>
+</div>
---
+++ src/components/Organisms/Home/CategoriesListing.svelte
@@ -0,0 +1,14 @@
+<script>
+  import CategoryCard from 'Molecules/CategoryCard';
+
+  export let cards;
+</script>
+
+<style>
+</style>
+
+<div class="row">
+  {#each cards as card, cardIndex (cardIndex)}
+    <CategoryCard {card} />
+  {/each}
+</div>
