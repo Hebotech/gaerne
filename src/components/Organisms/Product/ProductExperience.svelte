b7db10557c85f3ba34eecaaf4f07ba5d2475fae4
Irving Suárez <irving.dev@icloud.com>
2020-10-01 22:48:42 -0500

    Feature: Adding product view functions with product showcase

--- src/components/Organisms/Product/ProductExperience.svelte
+++ src/components/Organisms/Product/ProductExperience.svelte
@@ -4,12 +4,17 @@
   export let longDescription;
 
   $: imagesArray = images.map((image, imageIndex) => {
-    return { image, imageIndex };
+    return {
+      image,
+      imageIndex,
+    };
   });
 
   $: activeIndex = 0;
   $: activeImage = imagesArray[activeIndex];
-  $: inActiveImages = imagesArray.pop(activeIndex);
+  $: inActiveImages = imagesArray.filter(
+    (image) => image.imageIndex !== activeIndex
+  );
 
   function activateImage(imageIndex) {
     activeIndex = imageIndex;
@@ -18,25 +23,40 @@
 
 <style>
   .inactive-image {
-    padding: 25% 10;
+    padding: 25%;
+    width: 100%;
+    height: 100%;
+    background-size: contain;
+    cursor: pointer;
+    background-repeat: no-repeat;
+  }
+  .active-image {
+    background-repeat: no-repeat;
+    background-size: contain;
+    padding: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  .product-showcase {
+    display: flex;
+    flex-flow: nowrap row;
+    overflow: auto;
   }
 </style>
 
 <!-- markup (zero or more items) goes here -->
 
 <div class="row">
-  <div class="col-6">
-    <h3>
-      {shortDescription}
-    </h3>
+  <div class="col-5 text-center">
     <div class="product-showcase">
       {#each inActiveImages as image (image.imageIndex)}
-        <div on:click={activateImage(image.imageIndex)} class="inactive-image" style={`background-image: url(${{image}})`}/>
+        <div on:click={activateImage(image.imageIndex)} class="inactive-image" style={`background-image: url(${image.image})`}/>
       {/each}
     </div>
   </div>
-  <div class="col-6">
-    <div class="active-image" style={`background-image: url(${activeImage})`}/>
+  <div class="col-7">
+    <div class="active-image" style={`background-image: url(${activeImage.image})`}/>
   </div>
   <div class="col-12">
     <p>
--- src/views/Product.svelte
+++ src/views/Product.svelte
@@ -1,9 +1,23 @@
 <script>
   import { navigate } from 'svelte-routing';
+  import ProductExperience from 'Organisms/Product/ProductExperience';
+  import Categories from './Categories.svelte';
 
   export let name;
 
   $: categoryName = 'Enduro';
+  let product = {
+    name: 'SG-12',
+    shortDescription: 'Lo mejor que puede haber en la historia',
+    longDescription:
+      'GAERNE IS PROUD TO REPORT THAT MAGAZINES WORLD WIDE HAVE PUT OUR SG-12 THROUGH RIGORUS TESTS AND CAME BACK WITH 5 STAR REVIEWS CLAIMING IT THE MOST DURABLE, COMFORTABLE AND SUPPORTIVE BOOT MADE TO DATE.',
+    images: [
+      'http://www.gaerne.com/images/articoli/boots/2174_073.jpg',
+      'http://www.gaerne.com/images/articoli/boots/2174_079.jpg',
+      'http://www.gaerne.com/images/articoli/boots/2174_078.jpg',
+      'http://www.gaerne.com/images/articoli/boots/2174-053--sito.jpeg',
+    ],
+  };
 </script>
 
 <style>
@@ -35,6 +49,8 @@
     </div>
     <div class="col-12 text-center">
       <h1>{name}</h1>
+      <h3>{product.shortDescription}</h3>
     </div>
   </div>
+<ProductExperience {...product}/>
 </div>
