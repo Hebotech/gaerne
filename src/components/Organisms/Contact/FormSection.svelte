f4590b28466dcc9e01027bd5e048930f1a7a3e81
Irving Suárez <irving.dev@icloud.com>
2020-10-01 22:04:08 -0500

    Feature:Adding contact form

---
+++ src/components/Organisms/Contact/FormSection.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ContactForm from 'Molecules/Contact/ContactForm';
+</script>
+
+<style>
+  .bg-section {
+    background-image: url(/images/gaerne.jpg);
+    width: 100%;
+    background-size: cover;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
+<div class="row">
+  <div class="col-6 bg-sectio"/>
+  <div class="col-12">
+    <ContactForm/>
+  </div>
+</div>
--- src/views/Contact.svelte
+++ src/views/Contact.svelte
@@ -1,61 +1,34 @@
 <script>
-  // your script goes here
+  import FormSection from 'Organisms/Contact/FormSection';
 </script>
 
 <style>
-  /* your styles go here */
+  .contact-view {
+    background-color: #333;
+    color: white;
+    background-image: linear-gradient(
+        90deg,
+        rgba(0, 0, 0, 0.3) 0%,
+        rgba(0, 0, 0, 0.5) 100%
+      ),
+      url(http://www.gaerne.com/img_web/bg-contacts.jpg);
+    /* url(/images/gaerne.jpg); */
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .contact-view h1 {
+    color: #f4d316;
+    font-family: Cousine;
+  }
 </style>
 
 <!-- markup (zero or more items) goes here -->
-<div class="container-fluid text-center">
-  <div class="row">
+<div class="container-fluid p-5 text-center contact-view">
+  <div class="row m-0">
     <div class="col-12">
-      <h1>Contáctanos ahora mismo, perro!</h1>
-      <form>
-  <div class="form-row">
-    <div class="col-md-4 mb-3">
-      <label for="validationDefault01">First name</label>
-      <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationDefault02">Last name</label>
-      <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationDefaultUsername">Username</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupPrepend2">@</span>
-        </div>
-        <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
-      </div>
+      <h1>Contacto</h1>
     </div>
   </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationDefault03">City</label>
-      <input type="text" class="form-control" id="validationDefault03" placeholder="City" required>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationDefault04">State</label>
-      <input type="text" class="form-control" id="validationDefault04" placeholder="State" required>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationDefault05">Zip</label>
-      <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" required>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-      <label class="form-check-label" for="invalidCheck2">
-        Agree to terms and conditions
-      </label>
-    </div>
-  </div>
-  <button class="btn btn-primary" type="submit">Submit form</button>
-</form>
-
-    </div>
-  </div>
+  <FormSection/>
 </div>
