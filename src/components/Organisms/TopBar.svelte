32fb8325e19920c1a75d8ae9989d209f615890a4
Irving Suárez <irving.dev@icloud.com>
2020-10-31 13:18:33 -0600

    Improvement: Adding Typeform pop up to the TopBar component

--- src/components/Organisms/TopBar.svelte
+++ src/components/Organisms/TopBar.svelte
@@ -1,5 +1,27 @@
 <script>
   import { navigate } from 'svelte-routing';
+
+  $: {
+    (function () {
+      var qs,
+        js,
+        q,
+        s,
+        d = document,
+        gi = d.getElementById,
+        ce = d.createElement,
+        gt = d.getElementsByTagName,
+        id = 'typef_orm_share',
+        b = 'https://embed.typeform.com/';
+      if (!gi.call(d, id)) {
+        js = ce.call(d, 'script');
+        js.id = id;
+        js.src = b + 'embed.js';
+        q = gt.call(d, 'script')[0];
+        q.parentNode.insertBefore(js, q);
+      }
+    })();
+  }
 </script>
 
 <style>
@@ -39,7 +61,7 @@
     }
   }
 
-  .cta-column button {
+  .cta-column .button {
     border: none;
     font-family: Cousine;
 
@@ -52,7 +74,7 @@
     text-transform: uppercase;
   }
 
-  .cta-column button:hover {
+  .cta-column .button:hover {
     transition: all 0.3s ease-in;
     color: #333;
     background-color: #f5d315;
@@ -62,12 +84,20 @@
 <!-- markup (zero or more items) goes here -->
 
 <div class="logo-container row m-0 p-4">
-  <div class="col-md-6 col-12 d-flex justify-content-center my-md-0 my-3 justify-content-md-between">
-    <img on:click={()=>navigate('/')} src="/images/logo.png" alt="Logo Gaerne México" />
+  <div
+    class="col-md-6 col-12 d-flex justify-content-center my-md-0 my-3 justify-content-md-between">
+    <img
+      on:click={() => navigate('/')}
+      src="/images/logo.png"
+      alt="Logo Gaerne México" />
   </div>
-  <div class="col-md-6 col-12 cta-column d-flex flex-column align-items-md-end align-items-center justify-content-center" >
-    <button>
-      Ser distribuidor
-    </button>
+  <div
+    class="col-md-6 col-12 cta-column d-flex flex-column align-items-md-end align-items-center justify-content-center">
+    <a
+      class="typeform-share button"
+      href="https://form.typeform.com/to/KqufwcmS"
+      data-mode="popup"
+      target="_blank">Ser distribuidor
+    </a>
   </div>
 </div>
