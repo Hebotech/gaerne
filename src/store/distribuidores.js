8355c83c376bb5bd670cab1802c3cb3e9e966685
Irving Suárez <irving.dev@icloud.com>
2021-03-17 13:35:47 -0600

    Improvement: Filtering companies

--- src/store/distribuidores.js
+++ src/store/distribuidores.js
@@ -19,16 +19,17 @@ async function fetchProducts(set) {
       },
     } = await axios.get('https://apihebo.online/gaerne/companies');
 
-    let companiesArray = companies.map(({ properties: company }) => {
-      console.log(company);
+    let companiesArray = companies.filter((company)=>company.properties.gaerne && company.properties.website ?   company.properties.gaerne.value==='true' :false).map(({ properties: company }) => {
+      
       return {
-        name: company.name ? company.name.value : company.website.value,
-        website: company.website.value,
+        name: company.name ? company.name.value : company.website.value || 's',
+        website: company.website ? company.website.value: null,
         fav: company.fav ? company.fav.value : false,
       };
     });
 
-    console.log(companiesArray);
+    console.log(companiesArray)
+
 
     return set(companiesArray);
   } catch (error) {
